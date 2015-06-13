b44496f20ba48e2ce8f5db29686b94bf463884d8
Nemo <Nemo@yMBA.local>
2015-06-13 14:29:25 +0900

    minor fix

--- lib/numeric_with_unit.rb
+++ lib/numeric_with_unit.rb
@@ -4,24 +4,24 @@ require 'numeric_with_unit/unit'
 
 class NumericWithUnit
   include Comparable
-  
+
   attr_reader :value, :unit
-  
+
   def initialize(value, unit)
     @value = value
     @unit = unit.is_a?(Unit) ? unit : Unit[unit]
   end
-  
+
   # Return String for inspect
   def inspect
     "#{@value.inspect} [#{@unit.symbol}] #{unit.dimension.inspect}"
   end
-  
+
   # Return String with value and unit symbol
   def to_s
     "#{@value.to_s} #{@unit.symbol}"
   end
-  
+
   # If ohter is NumericWithUnit and same dimension, comparing value with converting to si.
   # Else return nil.
   def <=>(other)
@@ -29,44 +29,44 @@ class NumericWithUnit
       @unit.to_si(@value) <=> other.unit.to_si(other.value)
     end
   end
-  
+
   # Return succed value with same unit.
   def succ
     self.class.new(@value.succ, @unit)
   end
-  
+
   # Return value.to_i
   def to_i
     @value.to_i
   end
-  
+
   # Return value.to_f
   def to_f
     @value.to_f
   end
-  
+
   # Convert to given unit
   def convert(unit)
     new_unit = unit.is_a?(Unit) ? unit : Unit[unit]
-    
+
     unless @unit.dimension_equal? new_unit
       raise DimensionError, "Dimensions are different between #{@unit.symbol}#{@unit.dimension} #{new_unit.symbol}#{new_unit.dimension}"
     end
-    
+
     new_value = new_unit.from_si(@unit.to_si(@value))
     self.class.new(new_value, new_unit)
   end
   alias :to_nwu :convert
   alias :[] :convert
-  
+
   def +@
     self
   end
-  
+
   def -@
     self.class.new(-@value, @unit)
   end
-  
+
   def +(other)
     nwu = if other.is_a? self.class
       other
@@ -75,11 +75,11 @@ class NumericWithUnit
     end
     add_with_other_unit(nwu)
   end
-  
+
   def -(other)
     self + (-other)
   end
-  
+
   def *(other)
     case other
     when self.class
@@ -88,7 +88,7 @@ class NumericWithUnit
       self.class.new(@value*other, @unit)
     end
   end
-  
+
   def /(other)
     case other
     when self.class
@@ -97,7 +97,7 @@ class NumericWithUnit
       self.class.new(@value/other, @unit)
     end
   end
-  
+
   def coerce(other)
     if other.is_a?(self.class)
       [other, self]
@@ -105,7 +105,7 @@ class NumericWithUnit
       [self.class.new(other, Unit.new), self]
     end
   end
-  
+
   def **(num)
     # Dimension Check
     @unit.derivation.each do |k,v|
@@ -114,18 +114,18 @@ class NumericWithUnit
         raise DimensionError, "Dimension of #{k.symbol}(#{v}*#{num}) must be Integer"
       end
     end
-    
+
     self.class.new(@value**num, @unit**num)
   end
-  
+
   def root(num)
     self**(Rational(1,num))
   end
   def sqrt; root(2) end # 平方根
   def cbrt; root(3) end # 立方根
-  
+
   private
-  
+
   def add_with_other_unit(other)
     if @unit.dimension_equal? other.unit
       v1 = @unit.to_si(@value)
@@ -136,12 +136,12 @@ class NumericWithUnit
       raise DimensionError, "Dimensions are different between #{@unit.dimension} #{other.unit.dimension}"
     end
   end
-  
+
   def multiply_with_other_unit(other)
     onwu = adjust_other_unit(other)
     self.class.new(@value * onwu.value, @unit * onwu.unit)
   end
-  
+
   def devide_with_other_unit(other)
     onwu = adjust_other_unit(other)
     self.class.new(@value / onwu.value, @unit / onwu.unit)
@@ -159,7 +159,7 @@ class NumericWithUnit
       other
     end
   end
-  
+
 end
 
 
@@ -191,7 +191,7 @@ end
 class String
   def to_nwu(mthd=:to_r)
     # TODO: 適当なのでもう少しいい感じに。いい感じに
-    m = self.match /.*?(?=[\s\(\[])/
+    m = self.match(/.*?(?=[\s\(\[])/)
     value = m.to_s
     unit = m.post_match.strip.gsub(/^\[|\]$/, '')
     NumericWithUnit.new(value.__send__(mthd), unit)
@@ -203,4 +203,3 @@ end
 # unit definition
 require 'numeric_with_unit/unit_definition/base'
 require 'numeric_with_unit/unit_definition/common'
-
