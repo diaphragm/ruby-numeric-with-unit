c7b349b4b66c53edde52ca8f666f3bc3684268c8
diaphragm <diaphragm@users.noreply.github.com>
2016-02-10 21:17:14 +0900

    separate core_ext

--- lib/numeric_with_unit.rb
+++ lib/numeric_with_unit.rb
@@ -205,35 +205,7 @@ end
 
 
 
-class Fixnum
-  def to_nwu(unit)
-    NumericWithUnit.new(self, unit)
-  end
-end
-
-class Bignum
-  def to_nwu(unit)
-    NumericWithUnit.new(self, unit)
-  end
-end
-
-class Numeric
-  def to_nwu(unit)
-    NumericWithUnit.new(self, unit)
-  end
-end
-
-class String
-  def to_nwu(mthd=:to_r)
-    # TODO: 適当なのでもう少しいい感じに。いい感じに
-    m = self.match /.*?(?=[\s\(\[])/
-    value = m.to_s
-    unit = m.post_match.strip.gsub(/^\[|\]$/, '')
-    NumericWithUnit.new(value.__send__(mthd), unit)
-  end
-end
-
-
+require 'numeric_with_unit/core_ext'
 
 # unit definition
 require 'numeric_with_unit/unit_definition/base'
